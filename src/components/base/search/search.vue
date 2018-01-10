5beb8787b39ed879833d48ee6bbbb238f8c21176
dwt <15629135565@163.com>
2018-01-10 17:33:01 +0800

    xiaban

---
+++ src/components/base/search/search.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <i class="icon-search"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+    .icon-search{
+        font-size: 20px;
+        color: #000;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld1 from '@/components/HelloWorld';
 
 Vue.use(Router);
+
+// 路由懒加载
+const HelloWorld = () => import('@/components/HelloWorld');
 
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld1,
+      component: HelloWorld,
     },
   ],
 });
